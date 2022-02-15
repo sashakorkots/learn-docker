c8b7e0fb610bb06e6d31b147b5011314dc7afed0
Sasha Korkots <k.sasha.g@gmail.com>
2022-02-16 00:24:31 +0300

    Add lib path, create index.html

--- app/server.js
+++ app/server.js
@@ -4,14 +4,18 @@ import express from 'express';
 import moment from 'moment';   
 import dotenv from 'dotenv'
 import db from './db.js' 
+import path from 'path'
 
 dotenv.config()
 const port = process.env.PORT;
 
 const app = express();
-app.get('/', (req, res) => {
+app.get('/db', (req, res) => {
     db.query('INSERT INTO users (name, email) values ($1, $2)', ['Sasha', 'Korkots'])
-    res.send(`| (${moment().format('LLL')})`)
+    res.send(`database changed| (${moment().format('LLL')})`)
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve("./resorces/index.html"));
 });
 
 app.listen(port, () => {
