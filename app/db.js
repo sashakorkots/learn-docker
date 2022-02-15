c9c24a71b0eff5fd477d33339902074355c524e9
Oleksandr Korkots <oleksandrkor@interlink-ua.com>
2022-02-15 10:18:11 +0200

    Add volume to db, upadate project for linux

--- app/db.js
+++ app/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 const pool = new Pool({
     user: "test",
     password: 'secret',
-    host: "nodejs-db-1",
+    host: "learn-docker_db_1",
     port: 5432,
     database: "test"
 })
